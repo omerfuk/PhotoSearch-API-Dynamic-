127ddc32d22191dd4bc422790ec8e8f2f65f4da4
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-08 12:36:07 +0300

    .

--- PhotoSearch(API)(Dynamic)/ViewController.swift
+++ PhotoSearch(API)(Dynamic)/ViewController.swift
@@ -7,13 +7,129 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+struct APIResponse: Codable {
+    
+    let total: Int
+    let total_pages: Int
+    let results: [Result]
+    
+}
+
+struct Result: Codable {
+    
+    let id: String
+    let urls: URLS
+    
+}
+
+struct URLS: Codable {
+    
+    let regular: String
+}
+
+
+class ViewController: UIViewController, UICollectionViewDataSource, UISearchBarDelegate {
+
+    
+    
+    private var collectionView: UICollectionView?
+    
+    var results: [Result] = []
+    
+    let searchbar = UISearchBar()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(searchbar)
+        searchbar.delegate = self
+        
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        layout.itemSize = CGSize(width: view.frame.size.width/2, height: view.frame.size.width/2)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(ImageCollectionViewCell.self, forCellWithReuseIdentifier: ImageCollectionViewCell.identifier)
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .darkGray
+        view.addSubview(collectionView)
+        self.collectionView = collectionView
+                                        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+
+            //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+            //tap.cancelsTouchesInView = false
+
+            view.addGestureRecognizer(tap)
+        
+        
+    }
+        
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchbar.frame = CGRect(x: 10, y: view.safeAreaInsets.top, width: view.frame.size.width-20, height: 50)
+        collectionView?.frame = CGRect(x: 0, y: view.safeAreaInsets.top + 55, width: view.frame.size.width, height: view.frame.size.height - 55)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder() //REmoves keyboard
+        
+        if let text = searchbar.text {
+            results = []
+            collectionView?.reloadData()
+            fetchPhotos(query: text)
+        }
     }
 
+    
+    func fetchPhotos(query: String) {
+        let urlString = "https://api.unsplash.com/search/photos?page=1&per_page=50&query=\(query)&client_id=8v55atTFhjnu3fr-PT0cEYO8i86q5gf4mq_9W0yiF80"
+        
+        guard let url = URL(string: urlString) else {return}
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            
+            guard let data = data, error == nil else {return}
+            
+            do {
+                let jsonResult = try JSONDecoder().decode(APIResponse.self, from: data)
+                DispatchQueue.main.async {
+                    self?.results = jsonResult.results
+                    self?.collectionView?.reloadData()
+                }
+            } catch  {
+                print(error.localizedDescription)
+            }
+            
+        }
+        task.resume()
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let imageURLString = results[indexPath.row].urls.regular
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionViewCell.identifier, for: indexPath) as? ImageCollectionViewCell else{
+            return UICollectionViewCell()
+            
+        }
+        cell.configure(with: imageURLString)
+        
+        return cell
+    }
+        
 
 }
 
